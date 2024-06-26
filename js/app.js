801867dc27e9ae65ddaacba36ad7fb246f6cc87f
tupasj <53981391+tupasj@users.noreply.github.com>
2024-06-26 18:52:58 +0000

    Add menu console log

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var menuBtn = document.getElementsByClassName('menu-btn')
 var mobileMenu = document.getElementsByClassName('mobile-menu')
 var clickedBtn = function() {
   mobileMenu[0].classList.toggle('active')
+  console.log('toggle menu');
 }
 
 menuBtn[0].addEventListener('click', clickedBtn)
