1121903d81b1c10fd210fb5ad079640afd6b8fe2
tupasj <53981391+tupasj@users.noreply.github.com>
2024-06-26 18:40:11 +0000

    Change stylesheet href

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title></title>
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/css/main.css">
   </head>
   <body>
     <header>
